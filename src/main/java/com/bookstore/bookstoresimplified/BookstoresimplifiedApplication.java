d54c023b945bd6edcbf797fbce725bdeb5ccba4a
GuiTodys <guilhermetodys123@hotmail.com>
2022-03-31 11:48:15 -0300

    Project created

---
+++ src/main/java/com/bookstore/bookstoresimplified/BookstoresimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstoresimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoresimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoresimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore/bookstoresimplified/BookstoresimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstoresimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoresimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
